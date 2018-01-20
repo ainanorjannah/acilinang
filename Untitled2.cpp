15dce9f293def59395dbc8cab6424edb7ae5b3bc
ainanorjannah <ananorjannah0852@gmail.com>
2018-01-20 16:02:58 +0800

    mek bahan dan harga tas kulit

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -8,3 +8,14 @@ class tas{
 		string merk;
 		double harga;
 };
+int main () {
+	tas kulit;
+	
+	kulit.merk ="ripcurl";
+	kulit.harga = 20000;
+	
+	cout<<"merk yang dipakai "<< (kulit.merk)<<endl;
+	cout<<"Harga modal nya "<< (kulit.harga)<<endl;
+	
+	kulit.bahan("terbuat dari kulit buaya yang berkualitas tinggi");
+}
